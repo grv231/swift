9d98664d435ea2f6695909764da92d86fabbd103
Gaurav-Tripathi <gaurav.tripathi@tamu.edu>
2018-01-13 22:52:18 +0000

    changed the database host

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '192.168.33.30',
+	'hostname' => 'locahost',
 	'username' => 'ci_user',
 	'password' => 'cipassword',
 	'database' => 'ci_database',
